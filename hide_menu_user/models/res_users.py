9712c8bf3ba25d190a701db8ee34f740296e46b7
jbasiero <jbasiero@nextads.com>
2024-08-13 08:40:42 +0200

    [ADD] hide_menu_user

--- hide_menu_user/models/res_users.py
+++ hide_menu_user/models/res_users.py
@@ -3,7 +3,7 @@
 #
 #    Cybrosys Technologies Pvt. Ltd.
 #
-#    Copyright (C) 2021-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
 #    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
 #
 #    You can modify it under the terms of the GNU LESSER
@@ -19,50 +19,52 @@
 #    If not, see <http://www.gnu.org/licenses/>.
 #
 #############################################################################
+from odoo import fields, models
 
-from odoo import models, fields, api
 
-
-class HideMenuUser(models.Model):
+class ResUsers(models.Model):
+    """
+    Model to handle hiding specific menu items for certain users.
+    """
     _inherit = 'res.users'
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        """
-        Else the menu will be still hidden even after removing from the list
-        """
-        self.clear_caches()
-        return super(HideMenuUser, self).create(vals_list)
 
     def write(self, vals):
         """
-        Else the menu will be still hidden even after removing from the list
-        """
-        res = super(HideMenuUser, self).write(vals)
+         Write method for the ResUsers model.
+         Ensure the menu will not remain hidden after removing it from the list.
+           """
+        res = super(ResUsers, self).write(vals)
         for record in self:
             for menu in record.hide_menu_ids:
                 menu.write({
-                    'restrict_user_ids': [(4, record.id)]
+                    'restrict_user_ids': [fields.Command.link(record.id)]
                 })
-        self.clear_caches()
         return res
+
     def _get_is_admin(self):
         """
+        Compute method to check if the user is an admin.
         The Hide specific menu tab will be hidden for the Admin user form.
-        Else once the menu is hidden, it will be difficult to re-enable it.
         """
         for rec in self:
             rec.is_admin = False
             if rec.id == self.env.ref('base.user_admin').id:
                 rec.is_admin = True
 
-    hide_menu_ids = fields.Many2many('ir.ui.menu', string="Menu", store=True,
-                                     help='Select menu items that needs to be '
-                                          'hidden to this user ')
-    is_admin = fields.Boolean(compute=_get_is_admin, string="Admin")
+    hide_menu_ids = fields.Many2many(
+        'ir.ui.menu', string="Hidden Menu",
+        store=True, help='Select menu items that need to '
+                         'be hidden to this user.')
+    is_admin = fields.Boolean(compute=_get_is_admin, string="Is Admin",
+                              help='Check if the user is an admin.')
 
 
-class RestrictMenu(models.Model):
+class IrUiMenu(models.Model):
+    """
+    Model to restrict the menu for specific users.
+    """
     _inherit = 'ir.ui.menu'
 
-    restrict_user_ids = fields.Many2many('res.users')
+    restrict_user_ids = fields.Many2many(
+        'res.users', string="Restricted Users",
+        help='Users restricted from accessing this menu.')
